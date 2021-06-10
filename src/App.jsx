823b5586dfc9ae5aa05471b3fecdb50aebd537e7
Mcdaddy-2pointoh <sharvil.d@somaiya.edu>
2021-06-10 15:05:16 +0530

    Floating cards and Search functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
 import Data from "./Data";
 
 const App = () => {
+  const [input, changeInput] = useState("");
+  const [filterData, changeFilterData] = useState([]);
   const render = (obj, i) => {
     return (
       <Card
@@ -14,15 +16,32 @@ const App = () => {
       />
     );
   };
+  const search = (obj) => {
+    const val = obj.target.value;
+    changeInput(val);
+  };
+  useEffect(() => {
+    changeFilterData(
+      Data.filter((obj) => {
+        return obj.name.toLowerCase().includes(input.toLowerCase());
+      })
+    );
+  }, [input, changeFilterData, Data]);
+
   return (
     <>
       {" "}
       <div className="title_tile">
         {" "}
         Netflix Clone
-        <input type="text" placeholder="Search Your Show" />
+        <input
+          type="text"
+          placeholder="Search Your Show"
+          onChange={search}
+          value={input}
+        />
       </div>
-      <div className="obj">{Data.map(render)};</div>
+      <div className="obj">{filterData.map(render)};</div>
     </>
   );
 };
